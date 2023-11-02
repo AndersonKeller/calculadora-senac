745f54a2ae3cb80def3e4fa8bb878cb12bac5d00
AndersonKeller <andersonkellervendas@hotmail.com>
2023-11-01 21:50:20 -0300

    aula 01-11

---
+++ 01-11/products.js
@@ -0,0 +1,50 @@
+export const products = [
+    {
+        id:1,
+        name: "Televisão",
+        price: "900",
+        description: "televisão full HD 50 polegadas",
+        category: "Eletrônicos",
+        discount: "10"
+    },
+    {
+        id:2,
+        name: "Notebook",
+        price: "2400",
+        description: "notebook dell 17 polegadas, 128GB SSD, i5",
+        category: "Informática",
+        discount: "8"
+    },
+    {
+        id:3,
+        name: "Geladeira",
+        price: "2100",
+        description: "Geladeira Panasonic, 437 litros",
+        category: "Eletrodomésticos",
+        discount: "5"
+    },
+    {
+        id:4,
+        name: "Mesa de jantar",
+        price: "600",
+        description: "Mesa de jantar 5 cadeiras, kappesberg",
+        category: "Móveis",
+        discount: "0"
+    },
+    {
+        id:5,
+        name: "Sofá",
+        price: "2100",
+        description: "Sofá de canto, 3 lugares kappesberg",
+        category: "Móveis",
+        discount: "15"
+    },
+    {
+        id:6,
+        name: "Cafeteira",
+        price: "100",
+        description: "Cafeteira Philips, 30 xic. inox",
+        category: "Eletroportáteis",
+        discount: "2"
+    }
+]
---
+++ 01-11/scrip.js
@@ -0,0 +1,7 @@
+// listar os podutos em uma seção do site
+// eu como usuário quero poder adicionar um item ao carrinho
+// eu como usuário quero poder remover um item do carrinho
+//quero visualizar o valor total do carrinho
+// caso adicione um item repetido no carrinho, ele não deve ser criado
+//novamente dentro do carrinho, e sim exibir um contador mostrando
+//que existe mais de um desse item no carrinho
